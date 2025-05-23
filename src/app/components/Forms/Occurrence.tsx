c7e8ab43c579a530a37bfdbb8887f082b3a52c33
bruno <brunoazevedox@outlook.com>
2025-05-23 19:54:25 -0300

    update

--- src/app/components/Forms/Occurrence.tsx
+++ src/app/components/Forms/Occurrence.tsx
@@ -337,3 +337,15 @@ function CheckIcon(props: any) {
     </svg>
   );
 }
+<<<<<<< HEAD
+=======
+
+function formatPhoneNumber(phoneNumber: string) {
+  const cleaned = ("" + phoneNumber).replace(/\D/g, "");
+  const match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
+  return phoneNumber;
+}
+>>>>>>> 881d6f2 (update)
